ee64113bd93fe35e2e49df3d123a5919891c25ef
adarshgupta404 <gadarsh747@gmail.com>
2022-09-13 08:31:12 +0530

    minor changes

--- 01_bubblesort.cpp
+++ 01_bubblesort.cpp
@@ -2,12 +2,14 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+
 void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
+
 void bubbleSort(int arr[], int n)
 { 
     for(int i = 0; i<n; i++)
