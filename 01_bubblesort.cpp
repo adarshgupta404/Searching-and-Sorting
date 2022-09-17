594944deebeac24a13db4576e0d4f35812679fd5
adarshgupta404 <gadarsh747@gmail.com>
2022-09-17 21:21:49 +0530

    revising bubble sort

--- 01_bubblesort.cpp
+++ 01_bubblesort.cpp
@@ -10,19 +10,18 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-void bubbleSort(int arr[], int n)
-{ 
-    for(int i = 0; i<n; i++)
+void bubbleSort(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        for(int j = 0; j<n-i-1; j++)
+        for (int j = 0; j < n-i-1; j++)
         {
             if(arr[j+1]<arr[j])
-                {
-                    swap(&arr[j],&arr[j+1]);
-                }
-        }
+                swap(&arr[j+1], &arr[j]);
+        }  
     }
 }
+
 void display(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
